10d2c4cfc5a77466885b715b57eb533999f4eb07
Saloni <salonimehta2127@gmail.com>
2021-08-31 16:50:32 -0400

    completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves dwarf_names
+  dwarf_names.map.with_index(1) do |name,seq|
+    puts "#{seq}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |p|
+    "#{p.capitalize}!"
+  end
+end
+short_words = ["puff", "go", "two"]
+def long_planeteer_calls calls
+  calls.any? do |call|
+  call.length>4
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese food_list
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_list.find{|f| cheese_types.include?(f)}
 end
